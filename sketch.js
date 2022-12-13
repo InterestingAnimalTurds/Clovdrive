6fdcb56f8fb5b44c02851363db121408f430d77e
chongxu <chongxu.art@gmail.com>
2022-12-12 19:43:20 -0500

    first upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
+//state => display current files in cloud
+//state => upload the file 
+//state => check position and download
+//math => wind force to GPS position 
+//math => circle in GPS
+//resource => wind force
+//resource => gps library
+//resourece => file system IO
+
+
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400,400);
+  
 }
 
 function draw() {
   background(220);
+  text("upload your file\n get your cloud", 200, 200, 100, 100)
 }
